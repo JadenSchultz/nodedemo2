dcd027c64e088d141850c51e678143d4ce8903d6
Jaden <spacemanjaden125@gmail.com>
2022-11-01 15:37:29 -0600

    MERN 2 assignment for CSCI 331

---
+++ src/components/MyGitHub.jsx
@@ -0,0 +1,9 @@
+const MyGitHub = () => {
+    return (
+        <p>
+            Link to GitHub: <a href="https://github.com/JadenSchultz/nodedemo2">https://github.com/JadenSchultz/nodedemo2</a>
+        </p>
+    )
+}
+
+export default MyGitHub;
